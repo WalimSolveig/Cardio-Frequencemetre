786643e0746d0a373780607ab40e177bb8ea4955
Walim <machine-hb@hotmail.fr>
2017-11-16 14:39:38 +0100

    Module 1
    Code arduino pour prendre les battements.

---
+++ codeArduino.ino
@@ -0,0 +1,41 @@
+unsigned long time;
+int valeurPrecedente = 0;
+long tempsPrecedent = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Patientez un instant");
+  time = millis();
+
+  while((millis()-time)<1000) {
+    analogRead(0);
+  }
+
+  //afficher au moniteur série pendant 50s
+
+  while((millis()-time)< 50000){
+    Serial.println(analogRead(0));
+  }
+}
+
+void loop() {
+  int valeurActuelle, Seuil;
+  long tempsDetection;
+
+  valeurActuelle = analogRead(0);
+  Seuil = 650;
+
+  if (valeurActuelle > Seuil) {
+    if(valeurPrecedente <= Seuil) {
+      tempsDetection = millis();
+      if (tempsDetection > (tempsPrecedent + 200)) {
+        Serial.println( (100.0 * 60) / (tempsDetection - tempsPrecedent),0);
+        tempsPrecedent = tempsDetection;
+        
+      }
+    }
+  }
+  valeurPrecedente = valeurActuelle;
+}
+
