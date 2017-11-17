1faffcda14552057a6e9f5636361338daa1cf43b
Walim <machine-hb@hotmail.fr>
2017-11-17 19:45:03 +0100

    Update coeur.c

--- coeur.c
+++ coeur.c
@@ -7,7 +7,7 @@
  ########################################################################
 */
 
-void WriteOn()
+void WriteOn()  // Cette fonction n'est pas utilisée, elle sert juste d'exemple car elle permet d'écrire sur une fichier
 {
 	FILE* fichier = NULL;
     fichier = fopen("test.txt", "w");
@@ -16,7 +16,7 @@ void WriteOn()
     fputs("", fichier);
     fclose(fichier);
     }
-    return;
+    return WriteOn();
 }
 
 /*
@@ -26,37 +26,149 @@ void WriteOn()
 */
 
 
-int AllumageChenille()
+void AllumageChenille()
 {
 	FILE* fichier = NULL;
 	fichier = fopen("chenille.ino", "w");
 	if(fichier != NULL)
 	{
-     fputs("/*Allumage mode chenille*/\nint timer=300;\nvoidsetup(){\npinMode(3, OUTPUT);\npinMode(4, OUTPUT);\npinMode(5, OUTPUT);\npinMode(6, OUTPUT);\npinMode(7, OUTPUT);\npinMode(8, OUTPUT);\npinMode(9, OUTPUT);\npinMode(10, OUTPUT);\npinMode(11, OUTPUT);\npinMode(12, OUTPUT);\n}\nvoid loop() {\ndigitalWrite(3, HIGH);\ndelay(timer);\ndigitalWrite(3, LOW);\ndelay(timer);\ndigitalWrite(4, HIGH);\ndelay(timer);\ndigitalWrite(4, LOW);\ndelay(timer);\ndigitalWrite(5, HIGH);\ndelay(timer);\ndigitalWrite(5, LOW);\ndelay(timer);\ndigitalWrite(6, HIGH);\ndelay(timer);\ndigitalWrite(6, LOW);\ndelay(timer);\ndigitalWrite(7, HIGH);\ndelay(timer);\ndigitalWrite(7, LOW);\ndelay(timer);\ndigitalWrite(8, HIGH);\ndelay(timer);\ndigitalWrite(8, LOW);\ndelay(timer);\ndigitalWrite(9, HIGH);\ndelay(timer);\ndigitalWrite(9, LOW);\ndelay(timer);\ndigitalWrite(10, HIGH);\ndelay(timer);\ndigitalWrite(10, LOW);\ndelay(timer);\ndigitalWrite(11, HIGH);\ndelay(timer);\ndigitalWrite(11, LOW);\ndelay(timer);\ndigitalWrite(12, HIGH);\ndelay(timer);\ndigitalWrite(12, LOW);\ndelay(timer);\n", fichier};
+     fputs("int timer=300;\nvoidsetup(){\npinMode(3, OUTPUT);\npinMode(4, OUTPUT);\npinMode(5, OUTPUT);\npinMode(6, OUTPUT);\npinMode(7, OUTPUT);\npinMode(8, OUTPUT);\npinMode(9, OUTPUT);\npinMode(10, OUTPUT);\npinMode(11, OUTPUT);\npinMode(2, OUTPUT);\n}\nvoid loop() {\ndigitalWrite(2, HIGH);\ndelay(timer);\ndigitalWrite(3, HIGH);\ndelay(timer);\ndigitalWrite(3, LOW);\ndelay(timer);\ndigitalWrite(4, HIGH);\ndelay(timer);\ndigitalWrite(4, LOW);\ndelay(timer);\ndigitalWrite(5, HIGH);\ndelay(timer);\ndigitalWrite(5, LOW);\ndelay(timer);\ndigitalWrite(6, HIGH);\ndelay(timer);\ndigitalWrite(6, LOW);\ndelay(timer);\ndigitalWrite(7, HIGH);\ndelay(timer);\ndigitalWrite(7, LOW);\ndelay(timer);\ndigitalWrite(8, HIGH);\ndelay(timer);\ndigitalWrite(8, LOW);\ndelay(timer);\ndigitalWrite(9, HIGH);\ndelay(timer);\ndigitalWrite(9, LOW);\ndelay(timer);\ndigitalWrite(10, HIGH);\ndelay(timer);\ndigitalWrite(10, LOW);\ndelay(timer);\ndigitalWrite(11, HIGH);\ndelay(timer);\ndigitalWrite(11, LOW);\ndelay(timer);\ndigitalWrite(12, HIGH);\ndelay(timer);\ndigitalWrite(12, LOW);\ndelay(timer);\n", fichier);
      fclose(fichier);
     }
-   return 0;
+    return AllumageChenille();
 }
 
-int AllumageUnSurTrois()
+void AllumageUnSurTrois()
 {
     FILE* fichier = NULL;
     fichier = fopen("unsurtrois.ino", "w");
     if (fichier != NULL)
     {
-  fputs("/*Allumage une sur trois*/\nint timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(3, OUTPUT);\npinMode(4, OUTPUT);\npinMode(5, OUTPUT);\npinMode(6, OUTPUT);\npinMode(7, OUTPUT);\npinMode(8, OUTPUT);\npinMode(9, OUTPUT);\npinMode(10, OUTPUT);\npinMode(11, OUTPUT);\npinMode(12, OUTPUT);\n}\nvoid loop{\ndigitalWrite(3, HIGH);\ndelay(timer);\ndigitalWrite(3, LOW);\ndelay(timer);\ndigitalWrite(6, HIGH);\ndelay(timer);\ndigitalWrite(6, LOW);\ndelay(timer);\ndigitalWrite(9, HIGH);\ndelay(timer);\ndigitalWrite(9, LOW);\ndelay(timer);\ndigitalWrite(12, HIGH);\ndelay(timer);\ndigitalWrite(12, LOW);\ndelay(timer);\ndigitalWrite(4, HIGH);\ndelay(timer);\ndigitalWrite(4, LOW);\ndelay(timer);\ndigitalWrite(7, HIGH);\ndelay(timer);\ndigitalWrite(7, LOW);\ndelay(timer);\ndigitalWrite(10, HIGH);\ndelay(timer);\ndigitalWrite(10, LOW);\ndelay(timer);\ndigitalWrite(5, HIGH);\ndelay(timer);\ndigitalWrite(5, LOW);\ndelay(timer);\ndigitalWrite(8, HIGH);\ndelay(timer);\ndigitalWrite(8, LOW);\ndelay(timer);\ndigitalWrite(11, HIGH);\ndelay(timer);\ndigitalWrite(11, LOW);\ndelay(timer);\n}\n", fichier};
-  fclose(fichier);
+    fputs("/*Allumage une sur trois*/\nint timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(3, OUTPUT);\npinMode(4, OUTPUT);\npinMode(5, OUTPUT);\npinMode(6, OUTPUT);\npinMode(7, OUTPUT);\npinMode(8, OUTPUT);\npinMode(9, OUTPUT);\npinMode(10, OUTPUT);\npinMode(11, OUTPUT);\npinMode(2, OUTPUT);\n}\nvoid loop{\ndigitalWrite(3, HIGH);\ndelay(timer);\ndigitalWrite(3, LOW);\ndelay(timer);\ndigitalWrite(6, HIGH);\ndelay(timer);\ndigitalWrite(6, LOW);\ndelay(timer);\ndigitalWrite(9, HIGH);\ndelay(timer);\ndigitalWrite(9, LOW);\ndelay(timer);\ndigitalWrite(2, HIGH);\ndelay(timer);\ndigitalWrite(2, LOW);\ndelay(timer);\ndigitalWrite(4, HIGH);\ndelay(timer);\ndigitalWrite(4, LOW);\ndelay(timer);\ndigitalWrite(7, HIGH);\ndelay(timer);\ndigitalWrite(7, LOW);\ndelay(timer);\ndigitalWrite(10, HIGH);\ndelay(timer);\ndigitalWrite(10, LOW);\ndelay(timer);\ndigitalWrite(5, HIGH);\ndelay(timer);\ndigitalWrite(5, LOW);\ndelay(timer);\ndigitalWrite(8, HIGH);\ndelay(timer);\ndigitalWrite(8, LOW);\ndelay(timer);\ndigitalWrite(11, HIGH);\ndelay(timer);\ndigitalWrite(11, LOW);\ndelay(timer);\n}\n", fichier);
+    fclose(fichier);
     }
-  return 0;
+    return AllumageUnSurTrois();
 }
 
-int AllumageUnSurDeux(){
+void AllumageUnSurDeux(){
 	FILE* fichier = NULL;
     fichier = fopen("unsurdeux.ino", "w");
     if (fichier != NULL)
     {
-    fputs("/*Allumage une sur deux*/\nint timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(3, OUTPUT);\npinMode(4, OUTPUT);\npinMode(5, OUTPUT);\npinMode(6, OUTPUT);\npinMode(7, OUTPUT);\npinMode(8, OUTPUT);\npinMode(9, OUTPUT);\npinMode(10, OUTPUT);\npinMode(11, OUTPUT);\npinMode(12, OUTPUT);\n}\nvoid loop\n{\ndigitalWrite(4, HIGH);\ndelay(timer);\ndigitalWrite(4, LOW);\ndelay(timer);\ndigitalWrite(6, HIGH);\ndelay(timer);\ndigitalWrite(6, LOW);\ndelay(timer);\ndigitalWrite(8, HIGH);\ndelay(timer);\ndigitalWrite(8, LOW);\ndelay(timer);\ndigitalWrite(10, HIGH);\ndelay(timer);\ndigitalWrite(10, LOW);\ndelay(timer);\ndigitalWrite(12, HIGH);\ndelay(timer);\ndigitalWrite(12, LOW);\ndelay(timer);\ndigitalWrite(3, HIGH);\ndelay(timer);\ndigitalWrite(3, LOW);\ndelay(timer);\ndigitalWrite(5, HIGH);\ndelay(timer);\ndigitalWrite(5, LOW);\ndelay(timer);\ndigitalWrite(7, HIGH);\ndelay(timer);\ndigitalWrite(7, LOW);\ndelay(timer);\ndigitalWrite(9, HIGH);\ndelay(timer);\ndigitalWrite(9, LOW);\ndelay(timer);\ndigitalWrite(11, HIGH);\ndelay(timer);\ndigitalWrite(11, LOW);\ndelay(timer);\n}\n", fichier);
+    fputs("/*Allumage une sur deux*/\nint timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(3, OUTPUT);\npinMode(4, OUTPUT);\npinMode(5, OUTPUT);\npinMode(6, OUTPUT);\npinMode(7, OUTPUT);\npinMode(8, OUTPUT);\npinMode(9, OUTPUT);\npinMode(10, OUTPUT);\npinMode(11, OUTPUT);\npinMode(2, OUTPUT);\n}\nvoid loop\n{\ndigitalWrite(2, HIGH);\ndelay(timer);\ndigitalWrite(2, LOW);\ndelay(timer);\ndigitalWrite(4, HIGH);\ndelay(timer);\ndigitalWrite(4, LOW);\ndelay(timer);\ndigitalWrite(6, HIGH);\ndelay(timer);\ndigitalWrite(6, LOW);\ndelay(timer);\ndigitalWrite(8, HIGH);\ndelay(timer);\ndigitalWrite(8, LOW);\ndelay(timer);\ndigitalWrite(10, HIGH);\ndelay(timer);\ndigitalWrite(10, LOW);\ndelay(timer);\ndigitalWrite(3, HIGH);\ndelay(timer);\ndigitalWrite(3, LOW);\ndelay(timer);\ndigitalWrite(5, HIGH);\ndelay(timer);\ndigitalWrite(5, LOW);\ndelay(timer);\ndigitalWrite(7, HIGH);\ndelay(timer);\ndigitalWrite(7, LOW);\ndelay(timer);\ndigitalWrite(9, HIGH);\ndelay(timer);\ndigitalWrite(9, LOW);\ndelay(timer);\ndigitalWrite(11, HIGH);\ndelay(timer);\ndigitalWrite(11, LOW);\ndelay(timer);\n}\n", fichier);
 	fclose(fichier);
     }
-    return 0;
+    return AllumageUnSurDeux();
+}
+
+void AllumageLEDDeux(){
+	FILE* fichier = NULL;
+            fichier = fopen("led2.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(2, OUTPUT);\n}\nvoid loop{\ndigitalWrite(2, HIGH);\n}\n", fichier);
+            fclose(fichier);
+	        }
+	return AllumageLEDDeux();
+}
+
+void AllumageLEDTrois(){
+	FILE* fichier = NULL;
+            fichier = fopen("led3.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(3, OUTPUT);\n}\nvoid loop{\ndigitalWrite(3, HIGH);\n}\n", fichier);
+            fclose(fichier);
+        }
+    return AllumageUnSurTrois();
+}
+
+void AllumageLEDQuatre(){
+	FILE* fichier = NULL;
+            fichier = fopen("led3.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(4, OUTPUT);\n}\nvoid loop{\ndigitalWrite(4, HIGH);\n}\n", fichier);
+            fclose(fichier);
+	        }
+	return AllumageLEDQuatre();
+}
+
+void AllumageLEDCinq(){
+	FILE* fichier = NULL;
+            fichier = fopen("led4.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(5, OUTPUT);\n}\nvoid loop{\ndigitalWrite(5, HIGH);\n}\n", fichier);
+            fclose(fichier);
+	        }
+	return AllumageLEDCinq();
+}
+
+void AllumageLEDSix(){
+	
+	FILE* fichier = NULL;
+            fichier = fopen("led5.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(6, OUTPUT);\n}\nvoid loop{\ndigitalWrite(6, HIGH);\n}\n", fichier);
+            fclose(fichier);
+	        }
+	return AllumageLEDSix();
+}
+
+void AllumageLEDSept(){
+	FILE* fichier = NULL;
+            fichier = fopen("led6.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(7, OUTPUT);\n}\nvoid loop{\ndigitalWrite(7, HIGH);\n}\n", fichier);
+            fclose(fichier);
+	        }
+	return AllumageLEDSept();
+}
+
+void AllumageLEDHuit(){
+	FILE* fichier = NULL;
+            fichier = fopen("led7.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(8, OUTPUT);\n}\nvoid loop{\ndigitalWrite(8, HIGH);\n}\n", fichier);
+            fclose(fichier);
+	        }
+	return AllumageLEDHuit();
+}
+
+void AllumageLEDNeuf(){
+	FILE* fichier = NULL;
+            fichier = fopen("led8.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(9, OUTPUT);\n}\nvoid loop{\ndigitalWrite(9, HIGH);\n}\n", fichier);
+            fclose(fichier);
+	        }
+	return AllumageLEDNeuf();
+}
+
+void AllumageLEDDix(){
+	FILE* fichier = NULL;
+            fichier = fopen("led9.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(10, OUTPUT);\n}\nvoid loop{\ndigitalWrite(10, HIGH);\n}\n", fichier);
+            fclose(fichier);
+	        }
+	return AllumageLEDDix();
+}
+
+void AllumageLEDOnze(){
+	FILE* fichier = NULL;
+            fichier = fopen("led10.ino", "w");
+            if (fichier != NULL)
+            {
+            fputs("int timer=300;\nvoid setup() {\n/*Définir les OUTPOUT utilisés*/\npinMode(11, OUTPUT);\n}\nvoid loop{\ndigitalWrite(11, HIGH);\n}\n", fichier);
+            fclose(fichier);
+	        }
+
+    return AllumageLEDOnze();
 }
